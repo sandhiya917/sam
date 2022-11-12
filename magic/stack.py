2988a5b17a5f263e8b70ad1b7ab7e6ab45346ac3
sandhiya <sandhiya9676@gmail.com>
2022-11-12 12:04:47 +0530

    this is my first commit

---
+++ magic/stack.py
@@ -0,0 +1,8 @@
+stack=[]
+def push():
+    if len(stack)==n:
+        print(list is full)
+    else:
+        element=input("enter the element")
+        stack.append(element)
+        print(stack )
