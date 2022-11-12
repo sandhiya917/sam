9c2e7f2252a5ede39f279013330d8e3784548194
sandhiya <sandhiya9676@gmail.com>
2022-11-12 12:22:20 +0530

    this is my second commit

---
+++ lll.py
@@ -0,0 +1,8 @@
+import numpy as np
+import math
+in_array = [0,math.pi / 2 ,np.pi / 3, np.pi]
+print("input array: \n", in_array)
+Sin_values = np.sin(in_array)
+print("\nSin values: \n", Sin_values)
+cos_Values = np.cos(in_array)
+print("\nCos values: \n", cos_Values)
